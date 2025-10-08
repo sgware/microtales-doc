9e2ac6048a7bd46c51354b9203a2cb73a5118ac2
Stephen G. Ware <sgware@gmail.com>
2025-10-08 10:00:22 -0400

    Add *nix Compile Script
    Add compile shell script for Unix-style systems.

---
+++ compile.sh
@@ -0,0 +1,16 @@
+del microtales.pdf
+del docs/index.html
+pdflatex microtales.tex
+bibtex microtales
+pdflatex microtales.tex
+pdflatex microtales.tex
+pandoc microtales.tex -f latex -t html -s -o docs/index.html \
+--toc -V toc-title=Contents \
+--bibliography bibliography.bib --citeproc -M reference-section-title=Bibliography \
+--lua-filter codeenv.lua
+del microtales.aux
+del microtales.bbl
+del microtales.blg
+del microtales.log
+del microtales.out
+del microtales.toc
